28614e73a30c7024926f05171f6a8b603442b414
Alicia Sykes <alicia@omg.lol>
2025-04-19 14:44:13 +0100

    feat: Add support for custom events

--- src/app/services/hit-counting.service.ts
+++ src/app/services/hit-counting.service.ts
@@ -1,6 +1,34 @@
+/**
+ * HitCountingService - This uses a self-hosted instance of Plausible, to record
+ * certain events within the application.
+ * So that we can measure engagement so we can understand which features are most used,
+ * and improve accordingly.
+ * No personal data is collected, and no cookies are used.
+ * 
+ * All tracking will be skipped if:
+ * - The user has disabled analytics in the settings.
+ * - The user is not using the public managed instance.
+ * - The app is not running in the browser.
+ * - The browser has DNT enabled, or an ad-blocker.
+ * - The environmental variable to enable Plausible is not set.
+ */
 import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { EnvService } from '~/app/services/environment.service';
+import { SupabaseService } from '~/app/services/supabase.service';
+import { ErrorHandlerService } from '~/app/services/error-handler.service';
+
+type EventType =
+  'pageview_authenticated'
+  | 'pageview_unauthenticated'
+  | 'auth_view'
+  | 'auth_login_start'
+  | 'auth_login_done'
+  | 'auth_logout'
+  | 'auth_password_reset_start'
+  | 'auth_signup_start'
+  | 'auth_signup_done'
+  | 'add_domain';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +40,8 @@ export class HitCountingService {
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
     private envService: EnvService,
+    private supabaseService: SupabaseService,
+    private errorHandler: ErrorHandlerService,
   ) {
     this.plausibleEnabled = this.shouldEnablePlausible();
     if (this.plausibleEnabled) {
@@ -55,10 +85,70 @@ export class HitCountingService {
     document.head.appendChild(script);
   }
 
+  /* Checks auth state */
+  public async isAuthenticated(): Promise<boolean> {
+    try {
+      if (!this.envService.isSupabaseEnabled()) return false;
+      return await this.supabaseService.isAuthenticated()
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   * Checks if we should log event to Plausible
+   * Returns false if:
+   * - User disabled analytics
+   * - Not running in browser
+   * - Plausible script not loaded
+   * @returns true if all checks passed, false otherwise
+   */
+  public checkIfShouldContinue(): boolean {
+    // Cancel if user disabled analytics
+    if (!this.plausibleEnabled) {
+      return false;
+    }
+    
+    // Cancel if not client
+    if (!isPlatformBrowser(this.platformId) || typeof window === 'undefined') {
+      return false;
+    }
+
+    // Cancel if Plausible script is not loaded
+    if (typeof (window as any).plausible !== 'function') {
+      return false;
+    }
+    return true; // All checks passed, continue
+  }
+
+  /* Track a page view in Plausible */
+  public async trackPageView(url?: string) {
+    try {
+      if (!this.checkIfShouldContinue() || !url) return;
+      const isAuthenticated = await this.supabaseService.isAuthenticated();
+      const eventName = isAuthenticated ? 'pageview_authenticated' : 'pageview_unauthenticated';
+      const topLevel = (url.replace(/^\/+/, '').split('/')[0] || 'home');
+      (window as any).plausible(eventName, { props: { topLevel, url } });
+    } catch (error) {
+      this.errorHandler.handleError({
+        message: 'Unable to count page view',
+        error,
+        location: 'HitCountingService.trackPageView',
+      });
+    }
+  }
+
   /* Track a key event in Plausible */
-  public trackEvent(eventName: string, props?: Record<string, string>): void {
-    if (this.plausibleEnabled && isPlatformBrowser(this.platformId)) {
+  public trackEvent<E extends EventType>(eventName: E, props?: Record<string, any>) {
+    try {
+      if (!this.checkIfShouldContinue()) return;
       (window as any).plausible(eventName, { props });
+    } catch (error) {
+      this.errorHandler.handleError({
+        message: 'Unable to log event',
+        error,
+        location: 'HitCountingService.trackPageView',
+      });
     }
   }
 }
