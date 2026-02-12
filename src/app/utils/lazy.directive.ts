37e8e7b6c486d0d3b2cef8427a0c46adad54e1cd
Alicia Sykes <alicia@omg.lol>
2026-02-12 22:01:17 +0000

    Fixes intercation observer when running in SSR

--- src/app/utils/lazy.directive.ts
+++ src/app/utils/lazy.directive.ts
@@ -1,33 +1,41 @@
-import { Directive, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, Output, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Directive({
   selector: '[appLazyLoad]',
-  standalone: true, 
+  standalone: true,
 })
 export class LazyLoadDirective {
   @Output() visible = new EventEmitter<void>();
 
-  private observer: IntersectionObserver;
+  private observer?: IntersectionObserver;
 
-  constructor(private el: ElementRef) {
+  constructor(
+    private el: ElementRef,
+    @Inject(PLATFORM_ID) private platformId: any
+  ) {}
+
+  ngAfterViewInit(): void {
+    if (!isPlatformBrowser(this.platformId)) {
+      return;
+    }
+
     this.observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
             this.visible.emit();
-            this.observer.disconnect(); // Stop observing once it's visible
+            this.observer?.disconnect();
           }
         });
       },
-      { threshold: 0.1 } // Trigger when 10% of the element is visible
+      { threshold: 0.1 }
     );
-  }
 
-  ngAfterViewInit(): void {
     this.observer.observe(this.el.nativeElement);
   }
 
   ngOnDestroy(): void {
-    this.observer.disconnect();
+    this.observer?.disconnect();
   }
 }
